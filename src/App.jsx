745d88d7b0ba2a588f388a012978185362035f9c
logapriyanm <logapriyanvky@gmail.com>
2025-09-02 23:59:32 +0530

    Add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer'
+import Navbar from './Components/Navbar.jsx'
+import Footer from './Components/Footer'
 import Home from './Pages/Home'
 import Browse from './Pages/Browse'
 import PostBook from './Pages/PostBook'
